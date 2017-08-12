89c34fa0f7320eac01efe4a250f041102e9db51a
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-08-12 18:37:42 -0500

    organized file structure and added the packages

---
+++ routing/apiRoutes.js
@@ -0,0 +1,2 @@
+// get route with the url /api/friends which displays JSON data of possible friends
+// post routes /api/friends handle incoming survey results also used for compatibility logic
---
+++ routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+// get route to /survey
+// default use route that leads to home.html that displays the home page
